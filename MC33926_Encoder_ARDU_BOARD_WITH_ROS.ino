be259cdef1bb0de13c94a6dcc61b2e478b15edef
derkaputte <screen19@web.de>
2022-11-09 22:13:25 +0100

    Rewrite the test code in preparing for ros2 implementation

--- MC33926_Encoder_ARDU_BOARD_WITH_ROS.ino
+++ MC33926_Encoder_ARDU_BOARD_WITH_ROS.ino
@@ -1,14 +1,10 @@
 #include <Arduino.h>
+
 #include "RunningMedian.h"
 #include "TimerThree.h"
-#include <ros.h>
-#include <std_msgs/UInt16.h>
-#include <std_msgs/Float32.h>
-
-std_msgs::Float32 ticks_l;
-ros::NodeHandle  nh;
 
 #define pinMotorEnable  37         // EN motors enable
+
 #define pinMotorLeftPWM 5           // M1_IN1 left motor PWM pin
 #define pinMotorLeftDir 31          // M1_IN2 left motor Dir pin
 #define pinMotorLeftSense A1       // M1_FB  left motor current sense
@@ -22,6 +18,7 @@ ros::NodeHandle  nh;
 
 #define pinOdometryLeft A12      // left odometry sensor
 #define pinOdometryLeft2 A13     // left odometry sensor (optional two-wire)
+
 #define pinOdometryRight A14     // right odometry sensor 
 #define pinOdometryRight2 A15    // right odometry sensor (optional two-wire)  
 
@@ -42,18 +39,6 @@ boolean odometryRightLastState2;
     
 
 
-void motor_speed_l_cb( const std_msgs::UInt16& cmd_msg, int speed_l){
-  speed_l = cmd_msg.data; //set servo angle, should be from 0-180  
-}
-void motor_speed_r_cb( const std_msgs::UInt16& cmd_msg, int speed_r){
-  speed_r = cmd_msg.data; //set servo angle, should be from 0-180  
-}
-
-
-ros::Subscriber<std_msgs::UInt16> sub("speed_l", motor_speed_l_cb);
-ros::Subscriber<std_msgs::UInt16> sub("speed_r", motor_speed_r_cb);
-ros::Publisher pub_ticks_l("ticks_left", &ticks_l_msg);
-
 // Determines the rotation count and direction of the odometry encoders. Called in the odometry pins interrupt.
 // encoder signal/Ardumower pinout etc. at http://wiki.ardumower.de/index.php?title=Odometry
 // Logic is: 
@@ -62,6 +47,7 @@ ros::Publisher pub_ticks_l("ticks_left", &ticks_l_msg);
 //        Otherwise :  step count reverse   (odometryLeft--)   
 // odometryState:  1st left and right odometry signal
 // odometryState2: 2nd left and right odometry signal (optional two-wire encoders)
+
 void setOdometryState(unsigned long timeMicros, boolean odometryLeftState, boolean odometryRightState, boolean odometryLeftState2, boolean odometryRightState2){
   int leftStep = 1;
   int rightStep = 1;
@@ -116,20 +102,6 @@ ISR(PCINT2_vect, ISR_NOBLOCK){
 
 
 
-
-//#define pinOdometryLeft A12      // left odometry sensor
-
-
-// RunningMedian<int,50> measurements;
-// unsigned long ticks = 0;
-
-
-//ISR(PCINT2_vect){
-//  if (digitalRead(pinOdometryLeft)) ticks++;
-//}
-
-
-
 // MC33926 motor driver
 // Check http://forum.pololu.com/viewtopic.php?f=15&t=5272#p25031 for explanations.
 //(8-bit PWM=255, 10-bit PWM=1023)
@@ -137,36 +109,38 @@ ISR(PCINT2_vect, ISR_NOBLOCK){
 // PWM                L     Forward
 // nPWM               H     Reverse
 
-void setMC33926_l(int pinDir, int pinPWM, int speed_l) {
+void setMC33926(int pinDir, int pinPWM, int speed) {
+// void setMC33926_l(int pinDir, int pinPWM, int speed_l) {
   speed_l *= 4;
-  if (speed_l < 0) {
+  if (speed < 0) {
     digitalWrite(pinDir, HIGH ) ;
     //analogWrite(pinPWM, 255 - ((byte)abs(speed)));
-    Timer3.setPwmDuty(pinPWM, 1023 - abs(speed_l));
+    Timer3.setPwmDuty(pinPWM, 1023 - abs(speed));
   } else {
     digitalWrite(pinDir, LOW) ;
     //analogWrite(pinPWM, ((byte)speed));
-    Timer3.setPwmDuty(pinPWM, speed_l);
+    Timer3.setPwmDuty(pinPWM, speed);
   }
 }
 
-void setMC33926_r(int pinDir, int pinPWM, int speed_r) {
-  speed_r *= 4;
-  if (speed_r < 0) {
-    digitalWrite(pinDir, HIGH ) ;
-    //analogWrite(pinPWM, 255 - ((byte)abs(speed)));
-    Timer3.setPwmDuty(pinPWM, 1023 - abs(speed_r));
-  } else {
-    digitalWrite(pinDir, LOW) ;
-    //analogWrite(pinPWM, ((byte)speed));
-    Timer3.setPwmDuty(pinPWM, speed_r);
-  }
-}
+// void setMC33926_r(int pinDir, int pinPWM, int speed_r) {
+//   speed_r *= 4;
+//   if (speed_r < 0) {
+//     digitalWrite(pinDir, HIGH ) ;
+//     //analogWrite(pinPWM, 255 - ((byte)abs(speed)));
+//     Timer3.setPwmDuty(pinPWM, 1023 - abs(speed_r));
+//   } else {
+//     digitalWrite(pinDir, LOW) ;
+//     //analogWrite(pinPWM, ((byte)speed));
+//     Timer3.setPwmDuty(pinPWM, speed_r);
+//   }
+// }
 
 void setup()
 {
   Serial.begin(19200);
-   
+  Serial.println("START");  
+
   // motor enable
   pinMode(pinMotorEnable, OUTPUT);
   digitalWrite(pinMotorEnable, HIGH);
@@ -183,14 +157,10 @@ void setup()
   pinMode(pinMotorRightSense, INPUT);
   pinMode(pinMotorRightFault, INPUT);
 
-  pinMode(4, OUTPUT);
-  digitalWrite(4, HIGH);
+  // pinMode(4, OUTPUT);
+  // digitalWrite(4, HIGH);
  
-  nh.initNode();
-  nh.subscribe(sub);
-  nh.advertise(pub_ticks_l);
 
-  Serial.println("START");  
   
   // odometry
   pinMode(pinOdometryLeft, INPUT_PULLUP);  
@@ -228,94 +198,37 @@ void setup()
 void loop()
 {
   Serial.println("pwm_l,pwm_r,sense");
-  nh.spinOnce();
-  delay(1);
-  ticks_l_msg.data = odometryLeft;
-  pub_ticks_l.publish(&ticks_l_msg);
-}
-
-
-/*
-void beschleunigung() {
-    for (int speed=0; speed <= 125; speed += 10){    
+  unsigned long lastticks = 0;
+  for (int speed=0; speed <= 255; speed += 5){    
     setMC33926(pinMotorLeftDir, pinMotorLeftPWM, speed);
-    setMC33926(pinMotorRightDir, pinMotorRightPWM, speed);
-    delay(5);
+    delay(500);
+    lastticks = ticks;         
+    delay(1000);        
+    unsigned long stopticks = ticks;            
+    unsigned long stopTime = millis() + 1000;        
     for (int i=0; i < 10; i++){
+      //while (millis() < stopTime){
       int sense = 0;
       sense = analogRead(pinMotorLeftSense);
+      //measurements.clear();
+      /*for (int i=0; i < 100; i++){
+        sense = analogRead(pinMotorLeftSense);
+       measurements.add(sense);
+      }*/
+      //int fault = digitalRead(pinMotorLeftFault);      
       Serial.print(speed);
       Serial.print(",");    
+      Serial.print(stopticks-lastticks);
+      Serial.print(",");    
+      //float curr;
+      //measurements.getAverage(curr);
       Serial.print(sense);
-      Serial.print(",");    
-      Serial.print(odometryLeft);    
-      Serial.print(",");    
-      Serial.print(odometryRight);    
-      Serial.println();       
+      //Serial.print(",");    
+      //Serial.print(fault);
+      Serial.println();        
     }    
-  } 
-}
+  }   
+  setMC33926(pinMotorLeftDir, pinMotorLeftPWM, 0);
+  while (true);
 
-void bremsen() {
-    for (int speed=125; speed >= 0; speed -= 10){    
-    setMC33926(pinMotorLeftDir, pinMotorLeftPWM, speed);
-    setMC33926(pinMotorRightDir, pinMotorRightPWM, speed);
-    delay(5);
-    for (int i=0; i < 10; i++){
-      int sense = 0;
-      sense = analogRead(pinMotorLeftSense);
-      Serial.print(speed);
-      Serial.print(",");    
-      Serial.print(sense);
-      Serial.print(",");    
-      Serial.print(odometryLeft);    
-      Serial.print(",");    
-      Serial.print(odometryRight);
-      Serial.print(",");    
-      Serial.print(HIGH);    
-      Serial.println();       
-    }    
-  } 
 }
-
-void beschleunigung_z() {
-    for (int speed=0; speed >= -125; speed-= 10){    
-    setMC33926(pinMotorLeftDir, pinMotorLeftPWM, speed);
-    setMC33926(pinMotorRightDir, pinMotorRightPWM, speed);
-    delay(5);
-    for (int i=0; i < 10; i++){
-      int sense = 0;
-      sense = analogRead(pinMotorLeftSense);
-      Serial.print(speed);
-      Serial.print(",");    
-      Serial.print(sense);
-      Serial.print(",");    
-      Serial.print(odometryLeft);    
-      Serial.print(",");    
-      Serial.print(odometryRight);    
-      Serial.println();       
-    }    
-  } 
-}
-
-void bremsen_z() {
-    for (int speed=-125; speed <= 0; speed += 10){    
-    setMC33926(pinMotorLeftDir, pinMotorLeftPWM, speed);
-    setMC33926(pinMotorRightDir, pinMotorRightPWM, speed);
-    delay(5);
-    for (int i=0; i < 10; i++){
-      int sense = 0;
-      sense = analogRead(pinMotorLeftSense);
-      Serial.print(speed);
-      Serial.print(",");    
-      Serial.print(sense);
-      Serial.print(",");    
-      Serial.print(odometryLeft);    
-      Serial.print(",");    
-      Serial.print(odometryRight);    
-      Serial.println();       
-    }    
-  } 
-}
-
-*/
